bfe3c29f42a1a86349d37c9230839ca691e72cf8
Javier Ribó <elribonazo@gmail.com>
2025-04-19 22:50:29 +0200

    fix: unpack and send Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- src/core/Network.ts
+++ src/core/Network.ts
@@ -9,6 +9,7 @@ import { ProtocolMessage } from './message';
 import { PeerId } from '@libp2p/interface';
 import { getPeerIDDID } from './crypto';
 import { NodeServices } from '../services';
+import { Multiaddr } from '@multiformats/multiaddr';
 
 export class Network {
   private _mercury!: SDK.Mercury;
@@ -18,6 +19,11 @@ export class Network {
       throw new Error("Mercury not initialized");
     }
     return this._mercury;
+  }
+
+  get peer() {
+    if (!this.p2p) return null;
+    return this.p2p.peerId;
   }
 
 
@@ -91,6 +97,56 @@ export class Network {
     const result = await pipe([message], stream.sink);
     await stream.close({ signal: this.abortController.signal });
     return result;
+  }
+
+  public async dial(peer: any) {
+    return this.p2p.dial(peer, { signal: this.abortController.signal });
+  }
+
+  public async dialProtocol(
+    peer: PeerId | Multiaddr | Multiaddr[],
+    protocols: string | string[]
+  ) {
+    const dial = {
+      from: this.peer?.toString(),
+      to: peer.toString(),
+    };
+    console.log(
+      `[${JSON.stringify(protocols)}]: Dialing ${dial.from} to ${dial.to}`
+    );
+    const connection = await this.dial(peer);
+
+    return connection.newStream(protocols);
+  }
+
+  public async sendAndGetResponse(
+    peer: Multiaddr | Multiaddr[] | PeerId,
+    protocol: string,
+    message: Uint8Array
+  ): Promise<SDK.Domain.Message> {
+    try {
+      const stream = await this.dialProtocol(peer, protocol);
+      const results = new Promise<SDK.Domain.Message>((resolve, reject) => {
+        return pipe([message], stream, async (source) => {
+          try {
+            for await (const data of source) {
+              const unpacked = await this.unpackMessage(data.subarray());
+              return resolve(unpacked);
+            }
+          } catch (err) {
+            console.log(err);
+            return reject(err);
+          }
+        });
+      });
+
+      await results;
+      await stream.close({ signal: this.abortController.signal });
+      return results;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
   }
 
 }
